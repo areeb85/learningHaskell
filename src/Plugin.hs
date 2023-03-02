f6787ed23bc6169af1a272b5fac50754e07f5f31
areeb <areeb.shahid@u.yale-nus.edu.sg>
2023-03-02 22:56:53 +0800

    second round of optimisations

--- src/Plugin.hs
+++ src/Plugin.hs
@@ -3,21 +3,30 @@ import GHC.Plugins  hiding (substExpr, substTy, substCo, substTickish, substBndr
 import Prelude hiding (intercalate,map)
 import Data.List 
 import Data.Monoid (Alt, Alt)
-import GHC.Core.SimpleOpt
+import GHC.Core.SimpleOpt 
 import GHC.Core.FVs
 import GHC
 import GHC.Core
 import Data.String
 import GHC.Core.Utils (exprType)
 --import GhcPlugins
-import Data.Text hiding (intercalate,map, elem)
+import Data.Text hiding (intercalate,map, elem, zip, find, null)
 import System.Console.Isocline
 import Control.Monad.Trans (liftIO)
 import Control.Monad.Trans.Class (lift)
 --import System.Console.Haskeline
 import Control.Monad.Catch
 import Generics.SYB
+import GHC.Core.Opt.OccurAnal
+import GHC.Core.Opt.Specialise
+import GHC.Core.Rules
+import GHC.Builtin.PrimOps
+import GHC.Core.Tidy
+import qualified Data.Set as Set
 --import Linenoise
+
+
+import GHC.Core.Unfold
 
 
 
@@ -36,8 +45,11 @@ import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except (runExceptT)
 import Control.Monad.Trans.Writer (runWriterT)
 import Text.PrettyPrint.HughesPJ (Doc, render)
+import GHC.IO.Unsafe (unsafePerformIO)
 
 
+primOpIds :: Set.Set Id
+primOpIds = Set.fromList (map primOpWrapperId allThePrimOps)
 
 
 foldInts :: CoreExpr -> CoreM CoreExpr
@@ -83,16 +95,162 @@ loop guts dflags = do
       loop (guts { mg_binds = mg_binds' }) dflags
     Just "substitute" -> do
       let mg_binds' = everywhere (mkT letSubstTrivial) (mg_binds guts)
---      let mg_binds' = map (letSubstTrivial) (mg_binds guts)
---      mg_binds' <- mapM mapBinding (mg_binds guts)
-      loop (guts { mg_binds = mg_binds' }) dflags
+      let mg_binds'' = everywhere (mkT letrecSubstTrivialR) mg_binds'
+      loop (guts { mg_binds = mg_binds'' }) dflags
+    Just "simpleOpt" -> do
+      let unfoldingOpts =  updateCreationThreshold maxBound defaultUnfoldingOpts
+      let (mg_binds', rules', _) = GHC.Core.SimpleOpt.simpleOptPgm (GHC.Core.SimpleOpt.defaultSimpleOpts {so_uf_opts = unfoldingOpts}) (mg_module guts) (mg_binds guts) (mg_rules guts)
+--      liftIO $putStrLn (showSDoc dflags (ppr mg_binds'))
+--      liftIO $ putStrLn "----------------------------"
+--      liftIO $ putStrLn (showSDoc dflags (ppr rules'))
+--      liftIO $ putStrLn "----------------------------"
+--      liftIO $ putStrLn (showSDoc dflags  (ppr core_prgs))
+      loop (guts {mg_binds = mg_binds', mg_rules = rules'}) dflags
+    Just "occurence" -> do 
+      let mg_binds' = occurAnalysePgm (mg_module guts) (\_ -> True) (\_ -> False) (mg_rules guts) (mg_binds guts)
+      loop (guts {mg_binds = mg_binds'}) dflags
+    Just "specialize" -> do
+      guts' <- specProgram guts
+      loop guts' dflags
+    Just "printRules" -> do
+      liftIO $ putStrLn (showSDoc dflags (ppr (mg_rules  guts)))
+      liftIO $ putStrLn (showSDoc dflags (ppr (rulesOfBinds (mg_binds guts))))
+      loop guts dflags
+    Just "collectRules" -> do
+      let rules' = rulesOfBinds (mg_binds guts)
+      loop (guts {mg_rules = rules' ++ (mg_rules guts) }) dflags
+    Just "showUnfolding" -> do
+--      liftIO $ putStrLn $ (showPpr dflags allThePrimOps)
+      everywhereM (mkM (showUnfolding dflags)) (mg_binds guts)
+      loop guts dflags
+    Just "tidy" -> do
+      let go (NonRec b e) = NonRec b (tidyExpr emptyTidyEnv e)
+          go (Rec bes) = Rec [(b, tidyExpr emptyTidyEnv e) | (b, e) <- bes]
+      let binds = map go (mg_binds guts)
+      loop (guts { mg_binds = binds }) dflags
+    Just "force" -> do
+      let go (NonRec b e)
+           | "foo2" == occNameString (nameOccName (idName b)) = NonRec b (force dflags False e)
+          go e = e
+      let binds = map go (mg_binds guts)
+      loop (guts {mg_binds = binds}) dflags
+    Just "printUnfolding" -> do
+      everywhereM (mkM (printUnfoldings)) (mg_binds guts)
+      loop guts dflags
+    Just "printVarUnfolding" -> do
+      everywhereM (mkM (printVarUnfoldings)) (mg_binds guts)
+      loop guts dflags
     Just unknown -> do
       liftIO $ putStrLn ("unknown command <" ++ unknown ++ ">")
       loop guts dflags
-      
-     
-     
 
+printIdUnfoldings b = do
+  dflags <- getDynFlags
+  liftIO $ putStrLn $ showPpr dflags b ++ " = " ++ showPpr (gopt_unset dflags Opt_SuppressUnfoldings) (realIdUnfolding b)
+
+printUnfoldings (NonRec b e) = do
+  printIdUnfoldings b
+  return (NonRec b e)
+printUnfoldings (Rec bes) = do
+  dflags <- getDynFlags
+  mapM printIdUnfoldings $ map fst bes
+  return (Rec bes)
+printUnfoldings e = return e
+
+printVarUnfoldings :: CoreExpr -> CoreM CoreExpr
+printVarUnfoldings e@(Var i) = do
+  printIdUnfoldings i
+  return e
+printVarUnfoldings e = return e
+
+--updateUnfoldings :: CoreBinds -> CoreBinds
+--undateUnfoldings (NonRec b e) = NonRec b' e where
+--  b' = setIdUnfolding b unfolding
+--  unfolding = case realIdUnfolding of
+--
+--    mkUnfolding unfoldingOpts InlineRhs topLevel False e
+--  oldUnfolding = realIdUnfolding id
+
+
+
+
+force :: DynFlags -> Bool -> CoreExpr -> CoreExpr
+force dflags deep expr = unsafePerformIO $ do
+-- putStrLn "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+-- putStrLn $ showPpr dflags expr
+-- putStrLn ""
+ return $ case expr of
+  Var i -> unsafePerformIO $ do
+--    putStrLn $ showPpr dflags i ++ " = " ++ showPpr dflags (getUnfolding i)
+    return (getUnfolding i)
+  Lit _ -> expr
+  App fun arg -> App (force dflags deep  fun) (if deep || isPrimOpApp fun then force dflags True arg else arg)
+  Lam _ _ -> expr
+  Let bind expr -> Let bind (force dflags deep expr)
+  Case expr id ty alts -> Case (force dflags deep expr) id ty alts
+  Cast expr coerce -> Cast (force dflags deep expr) coerce
+  Tick tickish expr -> Tick tickish (force dflags deep expr)
+  Type ty -> expr
+  Coercion coerce -> expr
+
+
+isPrimOpApp :: CoreExpr -> Bool
+isPrimOpApp expr = case expr of
+  Var i ->  Set.member i primOpIds
+  App fun _ -> (isPrimOpApp fun)
+  _ -> False
+
+
+      
+
+
+showUnfolding :: DynFlags -> CoreExpr -> CoreM CoreExpr
+showUnfolding dflags (Var i) = do
+  liftIO $ putStrLn $ (showPpr dflags i) ++ " = " ++ (showPpr dflags (getUnfolding  i))
+  return (Var i)
+showUnfolding _ e = return e
+
+
+
+--specProgram' :: ModGuts -> CoreM ModGuts
+--specProgram' guts@(ModGuts { mg_module = this_mod
+--                          , mg_rules = local_rules
+--                          , mg_binds = binds })
+--  = do { dflags <- getDynFlags
+--
+--              -- We need to start with a Subst that knows all the things
+--              -- that are in scope, so that the substitution engine doesn't
+--              -- accidentally re-use a unique that's already in use
+--              -- Easiest thing is to do it all at once, as if all the top-level
+--              -- decls were mutually recursive
+--       ; let top_env = SE { se_subst = Core.mkEmptySubst $ mkInScopeSet $ mkVarSet $
+--                                       bindersOfBinds binds
+--                          , se_interesting = emptyVarSet
+--                          , se_module = this_mod
+--                          , se_dflags = dflags }
+--
+--             go []           = return ([], emptyUDs)
+--             go (bind:binds) = do (binds', uds) <- go binds
+--                                  (bind', uds') <- specBind top_env bind uds
+--                                  return (bind' ++ binds', uds')
+--
+--             -- Specialise the bindings of this module
+--       ; (binds', uds) <- runSpecM (go binds)
+--
+--       ; (spec_rules, spec_binds) <- specImports top_env local_rules uds
+--
+--       ; return (guts { mg_binds = spec_binds ++ binds'
+--                      , mg_rules = spec_rules ++ local_rules }) }
+
+
+
+
+getUnfolding :: Id -> CoreExpr
+getUnfolding i =
+  case unfoldingInfo (idInfo i) of
+    CoreUnfolding { uf_tmpl = uft } -> uft
+    dunf@(DFunUnfolding {})         -> mkCoreLams (df_bndrs dunf) $ mkCoreConApps (df_con dunf) (df_args dunf)
+    _                               -> Var i
 
 letSubstTrivial :: CoreExpr -> CoreExpr
 letSubstTrivial expr = case expr of
@@ -102,42 +260,62 @@ letSubstTrivial expr = case expr of
     
 
 
-substCoreExpr :: CoreBndr -> CoreExpr -> CoreExpr -> CoreExpr
-substCoreExpr bndr expr (Var id)
-  | id == bndr = expr
-  | otherwise = Var id
-substCoreExpr bndr expr (Lit lit) = Lit lit
-substCoreExpr bndr expr (App fun arg) =
-  App (substCoreExpr bndr expr fun) (substCoreExpr bndr expr arg)
-substCoreExpr bndr expr (Lam bndr' body)
-  | bndr' == bndr = Lam bndr' body
-  | otherwise =
-    let body' = substCoreExpr bndr expr body
-    in Lam bndr' body'
-substCoreExpr bndr expr (Let bind body) =
-  let bind' = case bind of
-                NonRec bndr' expr' ->
-                  if bndr' == bndr then
-                    NonRec bndr' expr'
-                  else
-                    NonRec bndr' (substCoreExpr bndr expr expr')
-                Rec pairs ->
-                  Rec [(bndr', substCoreExpr bndr expr expr')
-                      | (bndr', expr') <- pairs]
-  in Let bind' (substCoreExpr bndr expr body)
-substCoreExpr bndr expr (Case scrut bndr' ty alts) =
-  let alts' = map (\(con, vars, body') ->
-                      let vars' = if bndr' `elem` vars then vars else bndr' : vars
-                          body'' = substCoreExpr bndr expr body'
-                      in (con, vars', body'')
-                  ) alts
-      scrut' = substCoreExpr bndr expr scrut
-  in Case scrut' bndr' ty alts'
-substCoreExpr bndr expr (Cast e c) =
-  Cast (substCoreExpr bndr expr e) c
-substCoreExpr bndr expr (Tick t e) =
-  Tick t (substCoreExpr bndr expr e)
-substCoreExpr bndr expr (Type ty) = Type ty
+
+letrecSubstTrivialR :: CoreExpr -> CoreExpr
+letrecSubstTrivialR = everywhere (mkT subst)
+  where
+    subst (Let (Rec bs) e)
+      | Just (b, be, bs', e') <- findTrivial e [] bs =
+          let bs'' = map (substExprSC (extendSubst emptySubst b be) . snd) bs'
+              e'' = substExprSC (extendSubst emptySubst b be) e'
+          in Let (Rec (zip (map fst bs') bs'')) e''
+    subst expr = expr
+
+findTrivial _ _ [] = Nothing
+findTrivial e bs' ((b,be) : bs) =
+  case be of
+    Var _ -> Just (b, be, bs' ++ bs, e)
+    Lit _ -> Just (b, be, bs' ++ bs, e)
+    _ -> findTrivial e (bs'++[(b,be)]) bs
+
+
+
+--substCoreExpr :: CoreBndr -> CoreExpr -> CoreExpr -> CoreExpr
+--substCoreExpr bndr expr (Var id)
+--  | id == bndr = expr
+--  | otherwise = Var id
+--substCoreExpr bndr expr (Lit lit) = Lit lit
+--substCoreExpr bndr expr (App fun arg) =
+--  App (substCoreExpr bndr expr fun) (substCoreExpr bndr expr arg)
+--substCoreExpr bndr expr (Lam bndr' body)
+--  | bndr' == bndr = Lam bndr' body
+--  | otherwise =
+--    let body' = substCoreExpr bndr expr body
+--    in Lam bndr' body'
+--substCoreExpr bndr expr (Let bind body) =
+--  let bind' = case bind of
+--                NonRec bndr' expr' ->
+--                  if bndr' == bndr then
+--                    NonRec bndr' expr'
+--                  else
+--                    NonRec bndr' (substCoreExpr bndr expr expr')
+--                Rec pairs ->
+--                  Rec [(bndr', substCoreExpr bndr expr expr')
+--                      | (bndr', expr') <- pairs]
+--  in Let bind' (substCoreExpr bndr expr body)
+--substCoreExpr bndr expr (Case scrut bndr' ty alts) =
+--  let alts' = map (\(con, vars, body') ->
+--                      let vars' = if bndr' `elem` vars then vars else bndr' : vars
+--                          body'' = substCoreExpr bndr expr body'
+--                      in (con, vars', body'')
+--                  ) alts
+--      scrut' = substCoreExpr bndr expr scrut
+--  in Case scrut' bndr' ty alts'
+--substCoreExpr bndr expr (Cast e c) =
+--  Cast (substCoreExpr bndr expr e) c
+--substCoreExpr bndr expr (Tick t e) =
+--  Tick t (substCoreExpr bndr expr e)
+--substCoreExpr bndr expr (Type ty) = Type ty
 
 
 
@@ -149,62 +327,57 @@ plugin = defaultPlugin {
   }
 
 
--- commented the install function here.
-
---install :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
---install _ todo = do
---  return (CoreDoPluginPass "Say name" examplePass : todo)
 
 installTwo :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
 installTwo _ todo = do
   return (CoreDoPluginPass "Example Pass" examplePass : todo)
 
 
--- write recursive function here
-recursivePrinter :: DynFlags -> CoreExpr -> String
-recursivePrinter dflags (Var a) =  showSDoc dflags (ppr a)
-recursivePrinter dflags (Lit a) = showSDoc dflags (ppr a)
-recursivePrinter dflags (App e1 e2) = "( " ++ recursivePrinter dflags e1 ++ "@" ++ recursivePrinter dflags e2 ++ " )"
-recursivePrinter dflags (Lam b e) = "\\( " ++ showSDoc dflags (ppr b)  ++ "-> " ++ recursivePrinter dflags e ++ " )"
-recursivePrinter dflags (Let b e) = " Let " ++ recursiveBindPrinter dflags b ++ " in " ++  recursivePrinter dflags e
-recursivePrinter dflags (Case e b t as ) = "Case values are: " ++ recursivePrinter dflags e ++  " - " ++ showSDoc dflags (ppr b) ++ " - " ++ showSDoc dflags (ppr t) ++ " - " ++ recursiveCasePrinter dflags as
-recursivePrinter dflags (Cast b _) = "Cast values are: " ++ recursivePrinter dflags b 
-recursivePrinter dflags (Tick _ b) = " Tick values are: " ++ recursivePrinter dflags b
-recursivePrinter _ _ = "Not implemented"
+---- write recursive function here
+--recursivePrinter :: DynFlags -> CoreExpr -> String
+--recursivePrinter dflags (Var a) =  showSDoc dflags (ppr a)
+--recursivePrinter dflags (Lit a) = showSDoc dflags (ppr a)
+--recursivePrinter dflags (App e1 e2) = "( " ++ recursivePrinter dflags e1 ++ "@" ++ recursivePrinter dflags e2 ++ " )"
+--recursivePrinter dflags (Lam b e) = "\\( " ++ showSDoc dflags (ppr b)  ++ "-> " ++ recursivePrinter dflags e ++ " )"
+--recursivePrinter dflags (Let b e) = " Let " ++ recursiveBindPrinter dflags b ++ " in " ++  recursivePrinter dflags e
+--recursivePrinter dflags (Case e b t as ) = "Case values are: " ++ recursivePrinter dflags e ++  " - " ++ showSDoc dflags (ppr b) ++ " - " ++ showSDoc dflags (ppr t) ++ " - " ++ recursiveCasePrinter dflags as
+--recursivePrinter dflags (Cast b _) = "Cast values are: " ++ recursivePrinter dflags b 
+--recursivePrinter dflags (Tick _ b) = " Tick values are: " ++ recursivePrinter dflags b
+--recursivePrinter _ _ = "Not implemented"
+--
+--
+--recursiveCasePrinter :: DynFlags -> [CoreAlt] -> String
+--recursiveCasePrinter dflags alts = intercalate ", " (map (\(x,y,z) -> showSDoc dflags (ppr x) ++ " - " ++ showSDoc dflags (ppr y) ++ " - " ++ recursivePrinter dflags z) alts)
+--
+--
+--recursiveBindPrinter :: DynFlags -> CoreBind -> String
+--recursiveBindPrinter dflags (NonRec b e) = showSDoc dflags (ppr b)  ++ " = "  ++ recursivePrinter dflags e--(rewriteExpr caseReduceNoDC e)
+--recursiveBindPrinter dflags (Rec binds) = intercalate "," (map (\(b,e) -> showSDoc dflags (ppr b) ++ " = " ++ recursivePrinter dflags e) binds)
+--recursiveBindPrinter dflags _ = "nothing"
 
-
-recursiveCasePrinter :: DynFlags -> [CoreAlt] -> String
-recursiveCasePrinter dflags alts = intercalate ", " (map (\(x,y,z) -> showSDoc dflags (ppr x) ++ " - " ++ showSDoc dflags (ppr y) ++ " - " ++ recursivePrinter dflags z) alts)
-
-
-recursiveBindPrinter :: DynFlags -> CoreBind -> String
-recursiveBindPrinter dflags (NonRec b e) = showSDoc dflags (ppr b)  ++ " = "  ++ recursivePrinter dflags e--(rewriteExpr caseReduceNoDC e)
-recursiveBindPrinter dflags (Rec binds) = intercalate "," (map (\(b,e) -> showSDoc dflags (ppr b) ++ " = " ++ recursivePrinter dflags e) binds)
-recursiveBindPrinter dflags _ = "nothing"
-
-caseReduceNoDC :: CoreExpr -> CoreExpr -- this rewrite is merely for a boolean data constructor. 
-caseReduceNoDC c@(Case e _ _ as ) = 
-  let in_scope = mkInScopeSet (exprSomeFreeVars isLocalVar e) in 
-  case exprIsConApp_maybe (in_scope, idUnfolding) e of
-    Just (_, [], dc, [], [] ) -> case findAlt (DataAlt dc) as of
-      Nothing -> c
-      Just (_, _, rhs) -> rhs
-    _ -> c
-caseReduceNoDC e = e
+--caseReduceNoDC :: CoreExpr -> CoreExpr -- this rewrite is merely for a boolean data constructor. 
+--caseReduceNoDC c@(Case e _ _ as ) = 
+--  let in_scope = mkInScopeSet (exprSomeFreeVars isLocalVar e) in 
+--  case exprIsConApp_maybe (in_scope, idUnfolding) e of
+--    Just (_, [], dc, [], [] ) -> case findAlt (DataAlt dc) as of
+--      Nothing -> c
+--      Just (_, _, rhs) -> rhs
+--    _ -> c
+--caseReduceNoDC e = e
    
 
 
 
-rewriteExpr :: (CoreExpr -> CoreExpr) -> CoreExpr -> CoreExpr
-rewriteExpr f e@(Var a) = f e 
-rewriteExpr f e@(Lit a) = f e
-rewriteExpr f e@(App e1 e2) = f (App (rewriteExpr f e1) (rewriteExpr f e2))
-rewriteExpr f e@(Lam b body) = f (Lam b (rewriteExpr f body))
-rewriteExpr f e@(Let b body) = f (Let (rewriteBind f b) (rewriteExpr f body)) 
-rewriteExpr f e@(Case s b t as ) = f (Case (rewriteExpr f s) b t (rewriteAlts f as))
-rewriteExpr f e@(Cast e1 c) = f (Cast (rewriteExpr f e1) c) 
-rewriteExpr f e@(Tick t b) = f (Tick t (rewriteExpr f b))
-rewriteExpr f e = e
+--rewriteExpr :: (CoreExpr -> CoreExpr) -> CoreExpr -> CoreExpr
+--rewriteExpr f e@(Var a) = f e 
+--rewriteExpr f e@(Lit a) = f e
+--rewriteExpr f e@(App e1 e2) = f (App (rewriteExpr f e1) (rewriteExpr f e2))
+--rewriteExpr f e@(Lam b body) = f (Lam b (rewriteExpr f body))
+--rewriteExpr f e@(Let b body) = f (Let (rewriteBind f b) (rewriteExpr f body)) 
+--rewriteExpr f e@(Case s b t as ) = f (Case (rewriteExpr f s) b t (rewriteAlts f as))
+--rewriteExpr f e@(Cast e1 c) = f (Cast (rewriteExpr f e1) c) 
+--rewriteExpr f e@(Tick t b) = f (Tick t (rewriteExpr f b))
+--rewriteExpr f e = e
   
   
 --substExpr :: HasDebugCallStack => Subst -> CoreExpr -> CoreExpr
@@ -276,12 +449,12 @@ rewriteExpr f e = e
 
                    
 
-rewriteBind :: (CoreExpr -> CoreExpr) -> CoreBind -> CoreBind
-rewriteBind f (NonRec b e) = NonRec b (f e)
-rewriteBind f (Rec binds) = Rec (map (\(x, e) -> (x, (f e))) binds) 
+--rewriteBind :: (CoreExpr -> CoreExpr) -> CoreBind -> CoreBind
+--rewriteBind f (NonRec b e) = NonRec b (f e)
+--rewriteBind f (Rec binds) = Rec (map (\(x, e) -> (x, (f e))) binds) 
 
-rewriteAlts :: (CoreExpr -> CoreExpr) -> [CoreAlt] -> [CoreAlt]
-rewriteAlts f alts = map (\(dc, args, rhs) -> (dc, args, rewriteExpr f rhs)) alts
+--rewriteAlts :: (CoreExpr -> CoreExpr) -> [CoreAlt] -> [CoreAlt]
+--rewriteAlts f alts = map (\(dc, args, rhs) -> (dc, args, rewriteExpr f rhs)) alts
 
 pass :: ModGuts -> CoreM ModGuts
 pass guts = do dflags <- getDynFlags
@@ -291,7 +464,7 @@ pass guts = do dflags <- getDynFlags
         printBind dflags bndr =
           do
           -- call the recursive function here to print the values of underscore.
-          putMsgS $ "binding named " ++  recursiveBindPrinter dflags bndr
+--          putMsgS $ "binding named " ++  recursiveBindPrinter dflags bndr
           return bndr
         --printBind _ bndr = return bndr
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -O2 #-}
+{-# LANGUAGE BangPatterns #-}
+
+import Generics.SYB
+
 main :: IO ()
 
 
@@ -26,22 +33,61 @@ sampleMaybeBool2 val = case val of
   
 foldIntsTest = 3 + 4
 
-foo = let x = 4 in (show x) ++ (show x)
+foo = let x = 4 in 
+        let y = x in 
+          let z v = v + 1 in
+          show (z y) ++ show (z y)
+
+
+
+
+barInput :: [Int]
+barInput = [1, 2, 3, 4]
+{-# SPECIALIZE mySum :: [Int] -> Int#-}
+mySum :: (Num n) => [n] -> n
+mySum [] = 0
+mySum (x : y) = x + mySum y
+baz = mySum barInput
 
 --let f x = x + 1 in
 --  f 5
+
+--{-# SPECIALIZE sumSquares :: Int -> Int#-}
+sumSquares :: Int -> Int
+sumSquares n = go 0 1
+  where
+    go !acc !i
+      | i > n = acc
+      | otherwise = go (acc + i * i) (i + 1)
+
+
+
+--data Tree = Leaf | Branch Int Tree Tree deriving (Show, Eq, Ord, Typeable, Data)
+--
+--inc :: Tree -> Tree
+--inc = sybInc
+--
+--{-# SPECIALIZE sybInc :: Tree -> Tree#-}
+--sybInc :: (Data a) => a -> a
+--sybInc = everywhere (mkT f) where f (x :: Int) = x + 1
+--
+--exampleInput = Branch 1 (Branch 2 Leaf Leaf) (Branch 3 Leaf Leaf)
+--exampleOutput = inc exampleInput
   
   
+
 --data Salary = S Float
 --data Company = C [Dept]
-
-
+--
+--
 --incSal :: Float -> Salary -> Salary
 --incSal inc (S f) = S (inc * f)
 --
 --incSalSyb :: Company -> Company
 --incSalSyb = everywhere (mkt incSal) x
-
+foo2 = let f x = if x == 0 then 1 else x * g (x-1)
+           g x = if x == 0 then 1 else x * f (x-1) in 
+            f 5
 
   
-main = putStrLn ("Test suite not yet implemented haha" ++ show (factorial (foldIntsTest :: Int)) ++ sampleMaybeBool (Just True) ++ foo ++ foo)
+main = putStrLn ("Test suite not yet implemented haha" ++ show (factorial (foldIntsTest :: Int)) ++ sampleMaybeBool (Just True) ++ foo ++ foo ++ show baz ++ show (sumSquares 4) ++ show foo2)--show exampleOutput ++ )
